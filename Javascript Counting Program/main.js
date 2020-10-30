15b99d27ed55af651a898073a9aa23defa557092
jdeguzman2798 <67921714+jdeguzman2798@users.noreply.github.com>
2020-10-30 14:06:07 -0700

    Add files via upload

---
+++ Javascript Counting Program/main.js
@@ -0,0 +1,10 @@
+var name=prompt("What is your name?")
+alert("Hi " + name)
+
+var number=prompt("Enter a number:")
+alert("Hi " + name + ", the number you entered was " + number)
+
+for (i = 0; i <= number; i+=2) {
+$("h2").append(i + " ")
+}
+
